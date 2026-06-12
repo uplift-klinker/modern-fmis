8c952c323cf971934d4e352771ae4272d9cdbcfb
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2026-06-12 15:19:42 -0500

    Add test auth scheme and WebApplicationFactory for Api tests

---
+++ backend/tests/Fmis.Api.Tests/FmisApiFactory.cs
@@ -0,0 +1,27 @@
+using Fmis.Core;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Fmis.Api.Tests;
+
+public class FmisApiFactory : WebApplicationFactory<Program>
+{
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureTestServices(services =>
+        {
+            services.RemoveAll(typeof(DbContextOptions<FmisDbContext>));
+            services.AddDbContext<FmisDbContext>(options =>
+                options.UseInMemoryDatabase($"fmis-api-tests-{Guid.NewGuid()}"));
+
+            services.AddAuthentication(TestAuthHandler.SchemeName)
+                .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>(
+                    TestAuthHandler.SchemeName, _ => { });
+        });
+    }
+}
---
+++ backend/tests/Fmis.Api.Tests/TestAuthHandler.cs
@@ -0,0 +1,29 @@
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Fmis.Api.Tests;
+
+public class TestAuthHandler(
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+{
+    public const string SchemeName = "Test";
+
+    protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+    {
+        if (!Request.Headers.ContainsKey("Authorization"))
+        {
+            return Task.FromResult(AuthenticateResult.NoResult());
+        }
+
+        var claims = new[] { new Claim(ClaimTypes.NameIdentifier, "test-user") };
+        var identity = new ClaimsIdentity(claims, SchemeName);
+        var ticket = new AuthenticationTicket(new ClaimsPrincipal(identity), SchemeName);
+        return Task.FromResult(AuthenticateResult.Success(ticket));
+    }
+}
