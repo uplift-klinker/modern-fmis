55aec7f9baf9008b1ea7a2c9a8e0fc173d7ce153
Bryce Klinker <bryce.klinker@upliftdelivery.com>
2026-07-02 15:43:30 -0500

    Revert "Preview the application stack with placeholder ACR/identity values"
    This reverts commit e71bdf055dee07ef52f46275671c0b79e606b96b.

--- infra/Fmis.Infra.Common/StackReferenceExtensions.cs
+++ infra/Fmis.Infra.Common/StackReferenceExtensions.cs
@@ -4,13 +4,6 @@ namespace Fmis.Infra.Common;
 
 public static class StackReferenceExtensions
 {
-    public static Output<string> RequireString(this StackReference reference, string outputName, string previewFallback)
-    {
-        if (Deployment.Instance.IsDryRun)
-            return Output.Create(previewFallback);
-        return reference.RequireString(outputName);
-    }
-
     public static Output<string> RequireString(this StackReference reference, string outputName)
     {
         return reference.GetOutput(outputName).Apply(value =>
